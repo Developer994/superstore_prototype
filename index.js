1c1faaae837ff17a5059a20f4ba3fd73e3145746
Silent Dev <rez_a@outlook.com>
2019-11-12 18:59:59 -0500

    Started building the app. Now focusing on building the navbar.

---
+++ index.js
@@ -0,0 +1,3 @@
+// 1. Look at similar websites. See how they've laid out their components.
+// 2. a. Decide about what needs to be on the header and/or if a smart footer really needed. Start with a simple hello world.
+//    b. Start working with the top nav bar. What will the top nav have(flyer, searchbar, contact.)
