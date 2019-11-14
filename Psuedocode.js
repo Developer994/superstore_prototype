49b76b0f976c5429b0b824bf86a62d1aab16b186
Silent Dev <rez_a@outlook.com>
2019-11-14 16:06:13 -0500

    Re Cloned the files from github to fix navbar

---
+++ Psuedocode.js
@@ -0,0 +1,12 @@
+// 1. Look at similar websites. See how they've laid out their components.
+// 2. a. Decide about what needs to be on the header and/or if a smart footer really needed. Start with a simple hello world.
+//    b. Start working with the top nav bar. What will the top nav have(flyer, searchbar, contact.)
+// 3. Try to build the jumbotron:
+//    a. Build a box using with an id callled contentContainer
+//    b. Within contentContainer, create another div with class called textContainer
+//    c. Create an h1 text containing information about whatever will be below this contentContainer
+//    d. Create an h4 text for adding in more details about the h1 text.
+//    e. Style the contentContainer and position the textContainer, including the h1 and h4 elements.
+//    f. Add a catchy background image(The one that is used currently is only for testing purposes)
+// 4. Do media queries for the entire contentContainer.
+//    a. Target the contentContainer
