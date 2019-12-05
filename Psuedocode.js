334ad1f4d63a4fce0b5dd508b2f5e5388a528a50
Silent Dev <rez_a@outlook.com>
2019-12-05 14:21:33 -0500

    Finished working with the height media queries.

--- Psuedocode.js
+++ Psuedocode.js
@@ -1,3 +1,4 @@
+// Jumbotron & Navbar
 // 1. Look at similar websites. See how they've laid out their components.
 // 2. a. Decide about what needs to be on the header and/or if a smart footer really needed. Start with a simple hello world.
 //    b. Start working with the top nav bar. What will the top nav have(flyer, searchbar, contact.)
@@ -10,3 +11,7 @@
 //    f. Add a catchy background image(The one that is used currently is only for testing purposes)
 // 4. Do media queries for the entire contentContainer.
 //    a. Target the contentContainer
+
+
+// Contact Page Psuedocode
+// 1. Create a div and have a white background
